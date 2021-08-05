d078925825885f5956da652c470512ad27d1f6f9
sonagidevelop <jinh98326@gmail.com>
2021-08-05 13:56:37 +0900

    ㄲㅡㅌ
    구 나타내기까지 끝

--- lib/loading.dart
+++ lib/loading.dart
@@ -4,9 +4,6 @@ import 'package:http/http.dart';
 import 'dart:convert';
 
 class Loading extends StatefulWidget {
-
-
-
   @override
   _LoadingState createState() => _LoadingState();
 }
@@ -17,60 +14,103 @@ class _LoadingState extends State<Loading> {
     // TODO: implement initState
     super.initState();
     getLocation();
-
   }
+
   void getLocation() async {
-    try{
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    String lat = position.latitude.toString();
-    String lon = position.longitude.toString();
-    print(position);
-    print(lat);
-    print(lon); } catch(e) {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      String lat = position.latitude.toString();
+      String lon = position.longitude.toString();
+      print(position);
+      print(lat);
+      print(lon);
+    } catch (e) {
       print("there was an error");
     }
   }
 
-  void fetchData() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  Future<List> fetchData() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
     String lat = position.latitude.toString();
     String lon = position.longitude.toString();
-    lat = '${lat}'+'0';
-    lon = '${lon}'+'0';
+    lat = '${lat}' + '0';
+    lon = '${lon}' + '0';
     print(lat);
     print(lon);
 
-     Response response = await get(
-         Uri.parse("https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${lon}0,${lat}0&sourcecrs=epsg:4326&output=json"),
-       headers: test
-     );
-     String jsonData = response.body;
-     print(jsonData);
-     // var myJson_gu = jsonDecode(jsonData)["results"][1]['region']['area2']['name'];
-     // print(myJson_gu);
-     // var myJson_si = jsonDecode(jsonData)["results"][1]['region']['area1']['name'];
-     // print(myJson_si);
+    Response response = await get(
+        Uri.parse(
+            "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${lon}0,${lat}0&sourcecrs=epsg:4326&output=json"),
+        headers: test);
+    String jsonData = response.body;
+    print(jsonData);
+    var myJson_gu =
+        jsonDecode(jsonData)["results"][1]['region']['area2']['name'];
+    print(myJson_gu);
+    var myJson_si =
+        jsonDecode(jsonData)["results"][1]['region']['area1']['name'];
+    print(myJson_si);
+    List<String> gusi = [myJson_si, myJson_gu];
+
+    return gusi;
   }
 
   Map<String, String> test = {
-    "X-NCP-APIGW-API-KEY-ID" : "n9hamqjko2",
-    "X-NCP-APIGW-API-KEY" : "H0oYKsp8RuG0tn63J3geayID1VrBDVeeXuchqMSa"
+    "X-NCP-APIGW-API-KEY-ID": "n9hamqjko2",
+    "X-NCP-APIGW-API-KEY": "H0oYKsp8RuG0tn63J3geayID1VrBDVeeXuchqMSa"
   };
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            fetchData();
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // ElevatedButton(
+              //     onPressed: () {
+              //       fetchData();
+              //     },
+              //     child: Text('Get my location')),
+              FutureBuilder(
+                builder: (context, snapshot){
+                  if (snapshot.hasData == false) {
+                    return CircularProgressIndicator();
+                  }
+                  //error가 발생하게 될 경우 반환하게 되는 부분
+                  else if (snapshot.hasError) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Error: ${snapshot.error}',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    );
+                  }
+                  else {
+                    List ll = snapshot.data as List;
 
-          },
-          child: Text('Get my location'),
-        ),
-      ),
-    );
+                    return Padding(
+
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(ll[0]),
+                          Text("   "),
+                          Text(ll[1]),
+                        ],
+                      ),
+                    );
+                  }
+                },
+                future: fetchData(),
+              )
+            ],
+          ),
+        )
+        );
   }
 }
